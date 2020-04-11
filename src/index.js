c864042d150553a598c80f0880a04e1555c71a98
Alex Chavez <alex@alexchavez.net>
2020-04-11 01:39:01 -0700

    damn webpack and your css handling

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import _ from 'lodash';
 
 import './style.css';
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we\'re in development mode! Expect more output...');
+}
+
 function helloWorld() {
     console.log('Hello, world!');
 }
 
-if (process.env.NODE_ENV !== 'production') {
-    console.log('Looks like we\'re in development mode! Expect more output...');
-}
-else {
-    helloWorld();
-}
+helloWorld();
+
